1f3820631446135a912b2c0df1c9216ad5978979
Duvan Mesa <duvanmesa2415@gmail.com>
2024-10-01 16:32:55 -0500

    config.php

--- Infrastructure/Libs/Orm/Config.php
+++ Infrastructure/Libs/Orm/Config.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . "/activerecord/ActiveRecord.php";
 ActiveRecord\Config::initialize(function ($cfg) {
-  $cfg->set_model_directory($_SERVER["DOCUMENT_ROOT"] . "/guiastur/Domain/Entities");
+  $cfg->set_model_directory(__DIR__ . "/../../../Domain/Entities");
   $cfg->set_connections(
     array(
       'development' => 'mysql://root:@localhost/Gestion_turnos_guias_bd'
