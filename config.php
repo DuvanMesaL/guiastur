2b52ffe8bd5473c82409959b2a8e4855c8c096f1
Duvan Mesa <duvanmesa2415@gmail.com>
2024-10-01 16:01:47 -0500

    .env and composer vlucas/phpdotenv to get ROOT config

--- config.php
+++ config.php
@@ -1,5 +1,8 @@
 <?php
-// config.php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 if (getenv('DOCUMENT_ROOT')) {
     $_SERVER['DOCUMENT_ROOT'] = getenv('DOCUMENT_ROOT');
