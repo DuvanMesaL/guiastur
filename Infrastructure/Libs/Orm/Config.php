66e821705a534d90eb89c2d7a9f299c72c8468f3
Duvan Mesa <duvanmesa2415@gmail.com>
2024-10-01 19:16:23 -0500

    .env.example agregado

--- Infrastructure/Libs/Orm/Config.php
+++ Infrastructure/Libs/Orm/Config.php
@@ -9,24 +9,15 @@ if (getenv('APP_ENV') !== 'production') {
 ActiveRecord\Config::initialize(function ($cfg) {
   $cfg->set_model_directory(__DIR__ . "/../../../Domain/Entities");
 
-  if (getenv('APP_ENV') === 'production') {
-    // Usar las variables de entorno configuradas en Heroku
-    $db_host = getenv('DB_HOST_DEV');
-    $db_username = getenv('DB_USERNAME_DEV');
-    $db_password = getenv('DB_PASSWORD_DEV');
-    $db_name = getenv('DB_DATABASE_DEV');
-  } else {
-      // Configuración local
-      $db_host = 'localhost';
-      $db_username = 'root';
-      $db_password = 'tu_contraseña_local';
-      $db_name = 'Gestion_turnos_guias_bd';
-  }
+  $db_host = getenv('DB_HOST_DEV') ?: 'localhost';
+  $db_username = getenv('DB_USERNAME_DEV') ?: 'root';
+  $db_password = getenv('DB_PASSWORD_DEV') ?: '';
+  $db_name = getenv('DB_DATABASE_DEV') ?: 'Gestion_turnos_guias_bd';
 
   $cfg->set_connections(
     array(
-      'development' => 'mysql://root:@localhost/Gestion_turnos_guias_bd'
-      //'development' => 'mysql://' . $_ENV['DB_USERNAME'] . ':' . $_ENV['DB_PASSWORD'] . '@' . $_ENV['DB_HOST'] . '/' . $_ENV['DB_DATABASE']
+      //'development' => 'mysql://root:@localhost/Gestion_turnos_guias_bd'
+      'development' => 'mysql://' . $db_username . ':' . $db_password . '@' . $db_host . '/' . $db_name
       //    ,
       //    'test' => 'mysql://username:password@localhost/test_database_name',
       //    'production' => 'mysql://username:password@localhost/production_database_name'
