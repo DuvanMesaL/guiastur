180a0e55be9f94bf0a685d5f6e9142c088e0b9d1
Duvan Mesa <duvanmesa2415@gmail.com>
2024-10-01 19:06:24 -0500

    .env.example agregado

--- Infrastructure/Libs/Orm/Config.php
+++ Infrastructure/Libs/Orm/Config.php
@@ -9,8 +9,8 @@ ActiveRecord\Config::initialize(function ($cfg) {
   $cfg->set_model_directory(__DIR__ . "/../../../Domain/Entities");
   $cfg->set_connections(
     array(
-      //'development' => 'mysql://root:@localhost/Gestion_turnos_guias_bd',
-      'development' => 'mysql://' . $_ENV['DB_USERNAME'] . ':' . $_ENV['DB_PASSWORD'] . '@' . $_ENV['DB_HOST'] . '/' . $_ENV['DB_DATABASE']
+      'development' => 'mysql://root:@localhost/Gestion_turnos_guias_bd'
+      //'development' => 'mysql://' . $_ENV['DB_USERNAME'] . ':' . $_ENV['DB_PASSWORD'] . '@' . $_ENV['DB_HOST'] . '/' . $_ENV['DB_DATABASE']
       //    ,
       //    'test' => 'mysql://username:password@localhost/test_database_name',
       //    'production' => 'mysql://username:password@localhost/production_database_name'
