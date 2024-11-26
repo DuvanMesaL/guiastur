8ec06d2d17c77902e954919766aff7e8c001211a
Duvan Mesa <duvanmesa2415@gmail.com>
2024-11-26 16:51:18 -0500

    Usuarios y cros

--- api/routes/Atencions/CreateAtencion.php
+++ api/routes/Atencions/CreateAtencion.php
@@ -1,14 +1,13 @@
 <?php
 
-require_once $_SERVER["DOCUMENT_ROOT"] . "/guiastur/api/controllers/Atenciones/CreateAtencionMobileController.php";
+namespace Api\Routes\Endpoint\Atenciones;
 
-$createAtencionMobileController = new \CreateAtencionMobileController();
+require_once $_SERVER["DOCUMENT_ROOT"] . "/guiastur/api/controllers/Atenciones/CreateAtencionMobileController.php";
 
 $allowedOrigins = [
     "http://localhost:8100",
     "https://localhost:8100",
-    "http://192.168.137.57:8100",
-    "http://localhost:8100"
+    "http://192.168.137.57:8100"
 ];
 
 $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
@@ -26,10 +25,21 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header('Content-Type: application/json; charset=utf-8');
 
-if ($_SERVER['REQUEST_URI'] === '/api/atencion/create' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-    $createAtencionMobileController->handleRequest($_POST);
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+$createAtencionController = new \CreateAtencionMobileController();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $request = json_decode(file_get_contents('php://input'), true);
+    $createAtencionController->handleRequest($request);
 } else {
-    echo json_encode(['status' => 'error', 'message' => 'Ruta no encontrada']);
-    http_response_code(404);
+    http_response_code(405);
+    echo json_encode([
+        "status" => "error",
+        "message" => "Método no permitido"
+    ]);
     exit();
 }
